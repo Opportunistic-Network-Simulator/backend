2f1dd008a499d409ad5997375362b67847ed7fa2
bernardowerneck <bernardo.s.werneck@gmail.com>
2021-08-20 17:31:09 -0300

    avançando no parser

--- src/main/java/com/project/interfaces/commandLine/FileParser.java
+++ src/main/java/com/project/interfaces/commandLine/FileParser.java
@@ -26,25 +26,74 @@ public class FileParser {
     }
 
     private static MessageGenerationConfiguration parseMessageGenerationConfig(Toml parser) {
-        return new MessageGenerationConfiguration(
-                MessageGenerationType.valueOf(parser.getString("type")),
-                parser.getDouble("generationInstant"),
-                parser.getLong("amountNodes").intValue()
-            );
+        MessageGenerationConfiguration config = new MessageGenerationConfiguration();
+
+        MessageGenerationType type = MessageGenerationType.valueOf(parser.getString("type"));
+        config.setType(type);
+
+        String filename = parser.getString("filename");
+        if (filename != null) {
+            config.setFileName(filename);
+        }
+
+        Double generationInstant = parser.getDouble("generationInstant");
+        if (generationInstant != null) {
+            config.setGenerationInstant(generationInstant);
+        } else {
+            config.setGenerationInstant(0);
+        }
+
+        Long amountNodes = parser.getLong("amountNodes");
+        if (amountNodes != null) {
+            config.setAmountNodes(amountNodes.intValue());
+        }
+
+        Long sourceNodeId = parser.getLong("sourceNodeId");
+        if (sourceNodeId != null) {
+            config.setSourceNodeId(sourceNodeId.intValue());
+        }
+
+        Long destinationNodeId = parser.getLong("destinationNodeId");
+        if (destinationNodeId != null) {
+            config.setDestinationNodeId(destinationNodeId.intValue());
+        }
+
+        return config;
     }
 
     private static MeetingTraceConfiguration parseMeetingTraceConfig(Toml parser) {
+        MeetingTraceConfigurationType type = MeetingTraceConfigurationType.valueOf(parser.getString("type"));
+        String filename = parser.getString("filename");
+        Double totalSimulationTime = parser.getDouble("totalSimulationTime");
+
         return new MeetingTraceConfiguration(
-                MeetingTraceConfigurationType.valueOf(parser.getString("type")),
-                parser.getDouble("totalSimulationTime"),
-                parser.getString("filename")
+            MeetingTraceConfigurationType.valueOf(parser.getString("type")),
+            parser.getDouble("totalSimulationTime"),
+            parser.getString("filename")
         );
     }
 
     private static ProtocolConfiguration parseProtocolConfig(Toml parser) {
-        return new ProtocolConfiguration(
-                ProtocolType.valueOf(parser.getString("type")),
-                parser.getLong("l").intValue()
-        );
+        ProtocolConfiguration config = new ProtocolConfiguration();
+
+        ProtocolType type = ProtocolType.valueOf(parser.getString("type"));
+        config.setType(type);
+
+        Double p = parser.getDouble("p");
+        if (p != null) {
+            config.setP(p);
+        }
+
+        Double q = parser.getDouble("q");
+        if (q != null) {
+            config.setQ(q);
+        }
+
+        Long l = parser.getLong("l");
+        if (l != null) {
+            config.setL(l.intValue());
+        }
+
+        return config;
     }
 }
